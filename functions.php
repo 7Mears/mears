4dfeb78457455d5c9307cc0ee33b13e50fc40b7a
7Mears <7mears@gmail.com>
2015-01-27 22:55:40 -0600

    grunt uglify

--- functions.php
+++ functions.php
@@ -158,16 +158,11 @@ add_action( 'widgets_init', 'mears_widgets_init' );
 function mears_scripts() {
 	wp_enqueue_style( 'mears-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'mears-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20120206', true );
-
-	wp_enqueue_script( 'mears-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'mears-javascript', get_template_directory_uri() . '/js/script.min.js', array('jquery'), '20130115', true );
 
 	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Varela+Round|Fjalla+One');
 	wp_enqueue_style( 'googleFonts');
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'mears_scripts' );
 
--- gruntfile.js
+++ gruntfile.js
@@ -3,12 +3,32 @@ module.exports = function(grunt) {
   //Configure taksks
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    uglify : {
+
+      build : {
+        src: 'src/js/*.js',
+        dest: 'js/script.min.js'
+      },
+
+      dev: {
+        options: {
+          beautify: true,
+          mangle: false,
+          compress: false,
+          preserveComments: 'all'
+        },
+        src: 'src/js/*.js',
+        dest: 'js/script.min.js'
+      }
+    }
   });
-  
+
   // Load the plugins
-  grunt.loadNpmTasks( );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 
   // Register task
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['uglify:dev']);
+  grunt.registerTask('build', ['uglify:build']);
 
 };
